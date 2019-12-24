fb78e6646664bc8b6209a232050844cfd5fe9d60
ilg2005 <igoldfarb10@gmail.com>
2019-12-24 16:25:04 +0300

    текущий вариант

--- frontend/controllers/LocationController.php
+++ frontend/controllers/LocationController.php
@@ -3,15 +3,32 @@
 
 namespace frontend\controllers;
 
+require_once '../../functions.php';
 
 use frontend\models\Location;
 use yii\base\Controller;
 
 class LocationController extends Controller
 {
-   public function actionShow() {
-       $locations = Location::find()->asArray()->all();
-       return $this->render('show', compact('locations'));
-   }
+    public function actionImport()
+    {
+        $locations = getArrayFromCSV('../../data/cities.csv');
+        foreach ($locations as $key => $values) {
+            $location = new Location();
+            $location->id = $key++;
+            foreach ($values as $k => $v) {
+                $location->$k = $v;
+            }
+            $location->save();
+        }
+        $locations = Location::find()->asArray()->all();
+        return $this->render('import', compact('locations'));
+    }
+
+    public function actionShow()
+    {
+        $locations = Location::find()->asArray()->all();
+        return $this->render('show', compact('locations'));
+    }
 
 }
---
+++ frontend/views/location/import.php
@@ -0,0 +1,4 @@
+<?php
+?>
+<h1>Show Locations</h1>
+<?php var_dump($locations); ?>
