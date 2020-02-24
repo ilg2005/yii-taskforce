d6046460e7cc753cb4aa20130b9c888fb7a5f1ac
ilg2005 <igoldfarb10@gmail.com>
2020-02-24 19:50:34 +0300

    текущее состояние

--- frontend/controllers/StatisticsController.php
+++ frontend/controllers/StatisticsController.php
@@ -10,14 +10,10 @@ use yii\base\Controller;
 class StatisticsController extends Controller
 {
     public $layout = 'basic';
-    public static function getStatistics()
-    {
-        return Statistics::find()->asArray()->with('users')->all();
-    }
 
     public function actionShow()
     {
-        $statistics = self::getStatistics();
+        $statistics = Statistics::find()->asArray()->with('users')->all();
         return $this->render('show', compact('statistics'));
     }
 }
