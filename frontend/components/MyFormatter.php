b0f2f062f787ea6412117a1eb25d439762c88f7e
ilg2005 <igoldfarb10@gmail.com>
2020-03-03 21:20:09 +0300

    методы в MyFormatter приведены к виду asPhone, asAge

--- frontend/components/MyFormatter.php
+++ frontend/components/MyFormatter.php
@@ -9,13 +9,13 @@ use yii\i18n\Formatter;
 class MyFormatter extends Formatter
 {
 
-    public function formatAsPhone($number)
+    public function asPhone($number)
     {
         return preg_replace("/(\d)(\d{3})(\d{3})(\d{2})(\d{2})/",
             '$1 ($2) $3 $4 $5', $number);
     }
 
-    public function calculateAge($birthday)
+    public function asAge($birthday)
     {
         $birthdayTimestamp = strtotime($birthday);
         $age = date('Y') - date('Y', $birthdayTimestamp);
