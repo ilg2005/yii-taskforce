8fbf3d8956e226c9b84e86ca859c23f39bd8d85c
ilg2005 <igoldfarb10@gmail.com>
2020-03-31 12:16:25 +0300

    добавлена модальная форма входа в лэйаут basic.php

--- frontend/views/layouts/basic.php
+++ frontend/views/layouts/basic.php
@@ -167,6 +167,21 @@ BasicAsset::register($this);
             <?= ($this->title !== 'TaskForce-Signup') ? '' : \Yii::$app->view->renderFile('@app/views/taskforce-site/clipart.php') ?>
         </div>
     </footer>
+    <section class="modal enter-form form-modal" id="enter-form">
+        <h2>Вход на сайт</h2>
+        <form action="#" method="post">
+            <p>
+                <label class="form-modal-description" for="enter-email">Email</label>
+                <input class="enter-form-email input input-middle" type="email" name="enter-email" id="enter-email">
+            </p>
+            <p>
+                <label class="form-modal-description" for="enter-password">Пароль</label>
+                <input class="enter-form-email input input-middle" type="password" name="enter-email" id="enter-password">
+            </p>
+            <button class="button" type="submit">Войти</button>
+        </form>
+        <button class="form-modal-close" type="button">Закрыть</button>
+    </section>
 </div>
 <?php $this->endBody() ?>
 </body>
