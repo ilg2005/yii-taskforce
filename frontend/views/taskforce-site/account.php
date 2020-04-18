d6d218a6c77f9751a36d4f0bfa44ee55c06a974a
ilg2005 <igoldfarb10@gmail.com>
2020-04-18 18:50:58 +0300

    текущее состояние

--- frontend/views/taskforce-site/account.php
+++ frontend/views/taskforce-site/account.php
@@ -14,8 +14,9 @@ $this->title = 'TaskForce-Account';
                     <h3 class="div-line">Настройки аккаунта</h3>
                     <div class="account__redaction-section-wrapper">
                         <div class="account__redaction-avatar">
-                            <img src="../../../img/man-glasses.jpg" width="156" height="156">
-                            <a href="#" class="link-regular">Сменить аватар</a>
+                            <img src="./img/man-glasses.jpg" width="156" height="156">
+                            <input type="file" name="avatar" id="upload-avatar">
+                            <label for="upload-avatar" class="link-regular">Сменить аватар</label>
                         </div>
                         <div class="account__redaction">
                             <div class="account__input account__input--name">
@@ -26,13 +27,19 @@ $this->title = 'TaskForce-Account';
                                 <label for="201">email</label>
                                 <input class="input textarea" id="201" name="" placeholder="DenisT@bk.ru">
                             </div>
-                            <div class="account__input account__input--address">
-                                <label  for="202">Адрес</label>
-                                <input class="input textarea"  id="202" name="" placeholder="Санкт-Петербург, Московский район">
+                            <div class="account__input account__input--name">
+                                <label  for="202">Город</label>
+                                <select class="multiple-select input multiple-select-big" size="1" id="202" name="town[]">
+                                    <option value="Moscow">Москва</option>
+                                    <option selected="" value="SPB">Санкт-Петербург</option>
+                                    <option value="Krasnodar">Краснодар</option>
+                                    <option value="Irkutsk">Иркутск</option>
+                                    <option value="Vladivostok">Владивосток</option>
+                                </select>
                             </div>
                             <div class="account__input account__input--date">
                                 <label for="203">День рождения</label>
-                                <input id="203"  class="input-middle input input-date" type="text" placeholder="15.08.1987">
+                                <input id="203"  class="input-middle input input-date" type="date" placeholder="15.08.1987">
                             </div>
                             <div class="account__input account__input--info">
                                 <label for="204">Информация о себе</label>
@@ -42,21 +49,17 @@ $this->title = 'TaskForce-Account';
                     </div>
                     <h3 class="div-line">Выберите свои специализации</h3>
                     <div class="account__redaction-section-wrapper">
-                        <div class="search-task__categories account_checkbox">
-                            <input class="visually-hidden checkbox__input" id="205" type="checkbox" name="" value="">
+                        <div class="search-task__categories account_checkbox--bottom">
+                            <input class="visually-hidden checkbox__input" id="205" type="checkbox" name="" value="" checked>
                             <label for="205">Курьерские услуги</label>
-                            <input class="visually-hidden checkbox__input" id="206" type="checkbox" name="" value=""d>
-                            <label  for="206">Ремонт транспорта</label>
-                        </div>
-                        <div class="search-task__categories account_checkbox">
+                            <input class="visually-hidden checkbox__input" id="206" type="checkbox" name="" value="" checked>
+                            <label  for="206">Грузоперевозки</label>
                             <input class="visually-hidden checkbox__input" id="207" type="checkbox" name="" value="">
-                            <label for="207">Грузоперевозки</label>
-                            <input class="visually-hidden checkbox__input" id="208" type="checkbox" name="" value="">
-                            <label  for="208">Удалённая помощь</label>
-                        </div>
-                        <div class="search-task__categories account_checkbox">
-                            <input class="visually-hidden checkbox__input" id="209" type="checkbox" name="" value="" checked>
-                            <label for="209">Перевод текстов</label>
+                            <label for="207">Перевод текстов</label>
+                            <input class="visually-hidden checkbox__input" id="208" type="checkbox" name="" value="" checked>
+                            <label for="208">Ремонт транспорта</label>
+                            <input class="visually-hidden checkbox__input" id="209" type="checkbox" name="" value="">
+                            <label  for="209">Удалённая помощь</label>
                             <input class="visually-hidden checkbox__input" id="210" type="checkbox" name="" value="">
                             <label  for="210">Выезд на стрелку</label>
                         </div>
@@ -102,7 +105,7 @@ $this->title = 'TaskForce-Account';
                             <input class="visually-hidden checkbox__input" id="219" type="checkbox" name="" value="">
                             <label for="219">Показывать мои контакты только заказчику</label>
                             <input class="visually-hidden checkbox__input" id="220" type="checkbox" name="" value="" checked>
-                            <label  for="220">Показывать мои контакты только заказчику</label>
+                            <label  for="220">Не показывать мой профиль</label>
                         </div>
                     </div>
                 </div>
