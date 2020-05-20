08070e3a665ee27bb10ba181d87ca37e9be25ee4
ilg2005 <igoldfarb10@gmail.com>
2020-05-20 19:23:29 +0300

    текущее состояние

--- frontend/views/taskforce-site/account.php
+++ frontend/views/taskforce-site/account.php
@@ -136,10 +136,10 @@ $this->title = 'TaskForce-Account';
                 <h3 class="div-line">Фото работ</h3>
 
                 <div class="account__redaction-section-wrapper account__redaction">
-                    <span class="dropzone link-regular"></span>
-                       <?= $form->field($model, 'portfolio')
-                         ->label(false)
-                        ->fileInput(['hidden' => '', 'multiple' => true, 'class' => 'link-regular dropzone', 'name' => 'portfolio'])?>
+<!--                    <span class="dropzone link-regular"></span>
+-->                       <?= $form->field($model, 'portfolio[]')
+                         ->label('', ['class' => 'link-regular dropzone'])
+                        ->fileInput(['hidden' => '', 'multiple' => true, 'name' => 'portfolio']) ?>
                  </div>
 
                 <h3 class="div-line">Контакты</h3>
